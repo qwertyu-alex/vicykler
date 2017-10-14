7d15f3793500495b14969e8d11b3616603e55fe0
Alexander Le <alexandervanleprivat@gmail.com>
2017-10-14 15:51:27 +0200

    hello

---
+++ src/Hold.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Hold {
+    private String holdNavn;
+    private int antalHoldDeltagere;
+    private Virksomhed virksomhed;
+    private Deltager holdkaptajn;
+
+    private ArrayList <Deltager> deltagere;
+
+    private static int antalHold;
+
+    public void Hold(){
+    }
+
+    public void Hold(String holdNavn, int antalHoldDeltagere,
+                     Virksomhed virksomhed, Deltager deltager){
+
+        deltagere.add(deltager);
+
+    }
+
+
+
+
+}
