6b96b8de2069318a54c7161a82c56a6bf2cd852c
Alexander <alexandervanleprivat@gmail.com>
2017-10-15 12:32:02 +0200

    memes

--- src/Deltager.java
+++ src/Deltager.java
@@ -1,2 +1,73 @@
 public class Deltager {
+    //Variabler
+    private String cyklistType;
+    private String deltagerNavn;
+    private String deltagerEmail;
+    private Virksomhed virksomhed;
+    private Hold hold;
+    private int cykelDage;
+    private double antalKm;
+
+    private static int antalDeltagere = 0 ;
+    private static int antalCykelDageAlle = 0;
+    private static double antalKmAlle = 0;
+
+
+
+    //Constructor
+    public void Deltager(String cyklistType, String deltagerNavn, String deltagerEmail,
+                         Virksomhed virksomhed, Hold hold, int cykelDage, double antalKm){
+
+        //Variable definitioner
+        this.cyklistType = cyklistType;
+        this.deltagerNavn = deltagerNavn;
+        this.deltagerEmail = deltagerEmail;
+        this.virksomhed = virksomhed;
+        this.hold = hold;
+        this.cykelDage = cykelDage;
+        this.antalKm = antalKm;
+
+        //Static variabler definitioner
+        antalDeltagere++;
+        antalCykelDageAlle += cykelDage;
+    }
+
+
+    //Getter
+    public String getCyklistType() {
+        return cyklistType;
+    }
+
+    public String getDeltagerNavn() {
+        return deltagerNavn;
+    }
+
+    public String getDeltagerEmail() {
+        return deltagerEmail;
+    }
+
+    public String getInfo(){
+        //Kode mangler
+        return null;
+    }
+
+    //Static Getter
+    public static int getAntalDeltagere(){
+        //Kode mangler
+        return 0;
+    }
+
+
+    //Setter
+    public void setCyklistType(String cyklistType) {
+        this.cyklistType = cyklistType;
+    }
+
+    public void setDeltagerNavn(String deltagerNavn) {
+        this.deltagerNavn = deltagerNavn;
+    }
+
+    public void setDeltagerEmail(String deltagerEmail) {
+        this.deltagerEmail = deltagerEmail;
+    }
 }
