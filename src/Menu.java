4e825807dddbb91840623e0aaee69fb8134deeab
Alexander <alexandervanleprivat@gmail.com>
2017-11-07 13:28:15 +0100

    memes

--- src/Menu.java
+++ src/Menu.java
@@ -12,17 +12,17 @@ public class Menu {
 
     public void showAdminMenu(){
         System.out.println("Admin menu");
-        System.out.println("1) Tilføj firma");
-        System.out.println("2) Slet deltager");
-        System.out.println("3) Tilføj et hold");
-        System.out.println("4) Se information om en deltager");
-        System.out.println("5) Overblik over et firmas hold og tilhørende holdkaptajn");
-        System.out.println("6) Se information om et hold og dets deltagere");
-        System.out.println("7) Oplysninger om alle tilmeldte hold og deres deltagere");
-        System.out.println("8) Statistik over fordelingen af hold på firmaerne");
-        System.out.println("9) Ændre i et holds oplysninger");
-        System.out.println("10) Fjern deltager fra hold");
-        System.out.println("11) Slet hold");
+        System.out.println("1) Tilføj firma"); //tjek
+        System.out.println("2) Slet deltager"); //tjek
+        System.out.println("3) Tilføj et hold"); //tjek Opret hold
+        System.out.println("4) Se information om en deltager"); //tjek Oplysninger om deltager
+        System.out.println("5) Overblik over et firmas hold og tilhørende holdkaptajn"); //Oversigt over en virk hold og holdkaptajn
+        System.out.println("6) Se information om et hold og dets deltagere"); //tjek
+        System.out.println("7) Oplysninger om alle tilmeldte hold og deres deltagere"); //tjek
+        System.out.println("8) Statistik over fordelingen af hold på firmaerne"); //tjek
+        System.out.println("9) Ændre i et holds oplysninger");  //tjek
+        System.out.println("10) Fjern deltager fra hold"); //tjek
+        System.out.println("11) Slet hold");//tjek
 
 
         switch(input.nextInt()){
@@ -67,12 +67,12 @@ public class Menu {
 //Participant menu
     public void showParticpantsMenu(){
         System.out.println("Deltager menu");
-        System.out.println("1) Tilføj et hold");
-        System.out.println("2) Se information om en deltager");
-        System.out.println("3) Overblik over et firmas hold og tilhørende holdkaptajn");
-        System.out.println("4) Se information om et hold og dets deltagere");
-        System.out.println("5) Oplysninger om alle tilmeldte hold og deres deltagere");
-        System.out.println("6) Satestik over fordelingen af hold på firmaerne");
+        System.out.println("1) Tilføj et hold"); //opret hold tjek
+        System.out.println("2) Se information om en deltager"); //tjek
+        System.out.println("3) Overblik over et firmas hold og tilhørende holdkaptajn"); //tjek
+        System.out.println("4) Se information om et hold og dets deltagere"); //tjek
+        System.out.println("5) Oplysninger om alle tilmeldte hold og deres deltagere"); //tjek
+        System.out.println("6) Statestik over fordelingen af hold på firmaerne"); //tjek
 
         switch(input.nextInt()){
             case 1:
@@ -101,8 +101,9 @@ public class Menu {
 //Guest menu
     public void showGuestMenu(){
         System.out.println("Gæste menu");
-        System.out.println("1) Tilføj deltager");
-
+        System.out.println("Se statistikker");
+        System.out.println("Login");
+        System.out.println("Opret bruger");
         switch(input.nextInt()){
             case 1:
                 //indsæt metode
@@ -114,16 +115,15 @@ public class Menu {
    //tilføj alt det som deltager kan
     public void showTeamCaptainMenu(){
         System.out.println("Holdkaptajn menu");
-        System.out.println("1) Slet hold");
-        System.out.println("2) Tilføj deltager");
-        System.out.println("3) Fjern deltager fra hold");
-        System.out.println("4) Ændr i et holds oplysninger");
-        System.out.println("5) Tilføj et hold");
-        System.out.println("6) Se information om en deltager");
-        System.out.println("7) Overblik over et firmas hold og tilhørende holdkaptajn");
-        System.out.println("8) Se information om et hold og dets deltagere");
-        System.out.println("9) Oplysninger om alle tilmeldte hold og deres deltagere");
-        System.out.println("10) Satestik over fordelingen af hold på firmaerne");
+        System.out.println("1) Slet hold"); //tjek
+        System.out.println("2) Tilføj deltager til hold"); //tjek
+        System.out.println("3) Fjern deltager fra hold"); //tjek
+        System.out.println("4) Ændre i et holds oplysninger"); //tjek
+        System.out.println("5) Se information om en deltager"); //tjek
+        System.out.println("6) Overblik over et firmas hold og tilhørende holdkaptajn"); //tjek
+        System.out.println("7) Se information om et hold og dets deltagere"); //tjek
+        System.out.println("8) Oplysninger om alle tilmeldte hold og deres deltagere"); //tjek
+        System.out.println("9) Statistik over fordelingen af hold på firmaerne"); //tjek
 
         switch(input.nextInt()){
             case 1:
@@ -151,9 +151,6 @@ public class Menu {
                 //indsæt metode
                 break;
             case 9:
-                //indsæt metode
-                break;
-            case 10:
                 //indsæt metode
                 break;
             default:
